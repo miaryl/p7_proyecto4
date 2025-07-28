239a8b2b25a1021f597423fc6d7db7bae9f16068
Mio <gusw.0246.lpsyg@gmail.com>
2025-07-28 11:26:15 +0200

    eliminated reactRouter() in plugins in vite.config.js cos had error with it

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss(),reactRouter()],
+  plugins: [react(),tailwindcss()],
   test: {
     environment: 'jsdom',
     globals: true,
-  },
+  }
 });
