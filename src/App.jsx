84f936b29dda2d1b91a65261d919ae50ebd6706f
Abaraira <cibersteficodes@gmail.com>
2025-07-29 14:17:06 +0200

    started on forms

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './App.css'
-import ShowQuote from './components/quotes/Quotes.jsx'
 import Navbar from './components/navbar/NavBar.jsx'
 import AddQuote from './components/button/Button.jsx'
+import Form from './components/form/form.jsx'
+
 
 function App() {
 
@@ -12,8 +13,8 @@ function App() {
     <header>  <Navbar /> </header>
     <main className='mt-60 md:mt-30 lg:mt-60 flex flex-col items-center gap-4 min-w-[280px] sm:min-w-[320px]'>
       <h1 className='text-center text-4xl font-bold mb-8'> MOTIVA TU DÍA</h1>
-    <ShowQuote />
     <AddQuote />
+    <Form />
     </main>
          
     </>
--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -2,3 +2,62 @@ import React from "react";
 import { useEffect, useState } from "react";
 import "./form.css";
 
+function Form () {
+    const [quote, setQuote] = useState("");
+    const [author, setAuthor] = useState("");
+    const [randomQuote, setRandomQuote] = useState(null);
+    const [editingIndex, setEditingIndex] = useState(null);
+
+    useEffect(() => {
+        const storedQuotes = JSON.parse(localStorage.getItem("quotes")) || [];
+        setQuote(storedQuotes);
+        setRandomFrom(storedQuotes);
+ } 
+    , []);
+    const setRandomFrom = (quotes) => {
+        if (quotes.length > 0) {            
+            const randomIndex = quotes[Math.floor(Math.random() * quotes.length)];
+            setRandomQuote(randomIndex);
+        } else {
+            setRandomQuote(null);}
+        };
+        
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (quote && author) {
+        console.log(`Quote: ${quote}, Author: ${author}`);
+        setQuote("");
+        setAuthor("");
+        if (!text.trim ()) return;
+        const newQuote = text.trim();
+        const newAuthor = author.trim() || "Anónimo"
+      
+        
+    };
+    
+   
+}
+    
+    return (
+        <form onSubmit={handleSubmit} className="quote-form">
+        <input
+            type="text"
+            placeholder="Enter your quote"
+            value={quote}
+            onChange={(e) => setQuote(e.target.value)}
+            className="quote-input"
+            required
+        />
+        <input
+            type="text"
+            placeholder="Enter the author's name"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="author-input"
+        />
+        <button type="submit" className="submit-button">Add Quote</button>
+        </form>
+    );
+}
+export default Form;
+export { Form };
