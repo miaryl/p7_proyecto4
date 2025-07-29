dfe76a7d2faec43f056a4ade6f70932ab42a34da
Abaraira <cibersteficodes@gmail.com>
2025-07-29 13:25:19 +0200

    message

--- src/components/quotes/Quotes.jsx
+++ src/components/quotes/Quotes.jsx
@@ -1,12 +1,1 @@
 
-function ShowQuote() {
-    return (
-                   
-        <div>
-          <p className="flex flex-col justify-center gap-4 text-center text-2xl mb-8">La inspiración comienza con una sola frase.</p>
-        </div> 
-    )
-    
-}
-
-export default ShowQuote
