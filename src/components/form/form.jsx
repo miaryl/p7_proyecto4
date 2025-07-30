90eba85daf3088ea8192d401ec6763efc351d2df
Mio <gusw.0246.lpsyg@gmail.com>
2025-07-30 09:06:58 +0200

    cambie estilo de form.jsx

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -28,35 +28,43 @@ function Form () {
         console.log(`Quote: ${quote}, Author: ${author}`);
         setQuote("");
         setAuthor("");
-        if (!text.trim ()) return;
+        }
+        if (!text.trim()) {
         const newQuote = text.trim();
-        const newAuthor = author.trim() || "Anónimo"
+        const newAuthor = author.trim() || "Anónimo";
+
+        return newQuote, newAuthor
       
         
-    };
+        };
     
    
-}
+    }
     
     return (
-        <form onSubmit={handleSubmit} className="quote-form">
+        <>
+        <p className="mt-[500px]">Añade aquí tu frase</p>
+        <form onSubmit={handleSubmit} 
+        className="quote-form flex flex-col gap-4 w-full max-w-md">
         <input
             type="text"
-            placeholder="Enter your quote"
+            placeholder="Frase:"
             value={quote}
             onChange={(e) => setQuote(e.target.value)}
-            className="quote-input"
+            className="quote-input border rounded bg-white py-[50px] mb-9"
             required
         />
         <input
             type="text"
-            placeholder="Enter the author's name"
+            placeholder="Autor:"
             value={author}
             onChange={(e) => setAuthor(e.target.value)}
-            className="author-input"
+            className="author-input border rounded bg-white p-2 mb-9"
         />
-        <button type="submit" className="submit-button">Add Quote</button>
+        <button type="submit" 
+        className="rounded bg-white font-bold py-2 px-4 cursor-pointer hover:bg-gray-400">Añadir frase</button>
         </form>
+        </>
     );
 }
 export default Form;
