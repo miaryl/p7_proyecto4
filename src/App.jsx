875187d8e31d0d4ecceea4937f08c72b47e4c70e
surayac <surayacsm@gmail.com>
2025-07-30 12:02:44 +0200

    fixig bugs in app file

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Navbar from './components/navbar/NavBar.jsx'
 import AddQuote from './components/button/Button.jsx'
 import Form from './components/form/form.jsx'
+import ShowQuote from './components/quotes/Quotes.jsx'
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
     <header>  <Navbar /> </header>
     <main className='mt-60 md:mt-30 lg:mt-60 flex flex-col items-center gap-4 min-w-[280px] sm:min-w-[320px]'>
       <h1 className='text-center text-6xl font-bold mb-8'> MOTIVA TU DÍA</h1>
+      <ShowQuote />
     
     <AddQuote />
     <Form />
